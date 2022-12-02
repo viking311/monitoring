9218068c27d51030426855f8ce1d31c87f65302f
Rikhert Viktor Alexandrovich <viking311@gmail.com>
2022-12-02 15:51:59 +0700

    refactor storage

--- internal/storage/inmemory.go
+++ internal/storage/inmemory.go
@@ -17,10 +17,14 @@ func (ims *InMemoryStorage) Update(value entity.Metrics) {
 	ims.mx.Lock()
 	defer ims.mx.Unlock()
 
-	_, ok := ims.data[value.GetKey()]
-	if ok && value.MType == "counter" {
-		*ims.data[value.GetKey()].Delta += *value.Delta
-	} else {
+	if value.MType == "counter" {
+		_, ok := ims.data[value.GetKey()]
+		if ok && value.Delta != nil {
+			*ims.data[value.GetKey()].Delta += *value.Delta
+		} else if !ok && value.Delta != nil {
+			ims.data[value.GetKey()] = value
+		}
+	} else if value.MType == "gauge" && value.Value != nil {
 		ims.data[value.GetKey()] = value
 	}
 }
@@ -63,62 +67,3 @@ func NewInMemoryStorage() *InMemoryStorage {
 		mx:   sync.RWMutex{},
 	}
 }
-
-// type Repository interface {
-// 	Update(value entity.MetricEntityInterface)
-// 	Delete(key string)
-// 	GetByKey(key string) entity.MetricEntityInterface
-// 	GetAll() []entity.MetricEntityInterface
-// }
-
-// type InMemoryStorage struct {
-// 	data map[string]entity.MetricEntityInterface
-// 	mx   sync.RWMutex
-// }
-
-// func (ims *InMemoryStorage) Update(value entity.MetricEntityInterface) {
-// 	ims.mx.Lock()
-// 	defer ims.mx.Unlock()
-// 	item, ok := ims.data[value.GetKey()]
-// 	if ok {
-// 		item.SetValue(value.GetValue())
-// 		ims.data[value.GetKey()] = item
-// 	} else {
-// 		ims.data[value.GetKey()] = value
-// 	}
-// }
-
-// func (ims *InMemoryStorage) Delete(key string) {
-// 	ims.mx.Lock()
-// 	defer ims.mx.Unlock()
-// 	delete(ims.data, key)
-// }
-
-// func (ims *InMemoryStorage) GetByKey(key string) entity.MetricEntityInterface {
-// 	ims.mx.RLock()
-// 	defer ims.mx.RUnlock()
-// 	value, ok := ims.data[strings.ToLower(key)]
-// 	if ok {
-// 		return value
-// 	} else {
-// 		return nil
-// 	}
-// }
-
-// func (ims *InMemoryStorage) GetAll() []entity.MetricEntityInterface {
-// 	ims.mx.RLock()
-// 	defer ims.mx.RUnlock()
-// 	slice := make([]entity.MetricEntityInterface, len(ims.data))
-// 	for _, item := range ims.data {
-// 		slice = append(slice, item)
-// 	}
-
-// 	return slice
-// }
-
-// func NewInMemoryStorage() *InMemoryStorage {
-// 	return &InMemoryStorage{
-// 		data: make(map[string]entity.MetricEntityInterface),
-// 		mx:   sync.RWMutex{},
-// 	}
-// }
