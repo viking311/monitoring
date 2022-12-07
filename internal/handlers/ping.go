7c6bb665ee9808c6816b5bff7c5b796add666b74
Rikhert Viktor Alexandrovich <viking311@gmail.com>
2022-12-07 12:25:07 +0700

    added ping handler draft

---
+++ internal/handlers/ping.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type PingHandler struct {
+	Server
+}
+
+func (ph PingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if ph.db == nil {
+		fmt.Println("Db nuil")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	if err := ph.db.PingContext(ctx); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func NewPingHandler(db *sql.DB) *PingHandler {
+	return &PingHandler{
+		Server: Server{
+			db: db,
+		},
+	}
+}
