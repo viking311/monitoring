e11c4885a856b1017a8730bfd561a53878add8a1
Rikhert Viktor Alexandrovich <viking311@gmail.com>
2022-12-08 11:33:17 +0700

    refactored snapshot writer

--- internal/storage/snapshot_factory.go
+++ internal/storage/snapshot_factory.go
@@ -7,13 +7,14 @@ import (
 )
 
 func NewSnapshotInstance(config *server.ServerConfig, db *sql.DB, store Repository) (SnapshotWriterInterface, error) {
-	if len(*config.DatabaseDsn) > 0 {
-		sdw, err := NewSnapshotDbWriter(db, store, *config.StoreInterval)
-		if err != nil {
-			return nil, err
-		}
-		return sdw, nil
-	} else if len(*server.Config.StoreFile) > 0 {
+	// if len(*config.DatabaseDsn) > 0 {
+	// 	sdw, err := NewSnapshotDbWriter(db, store, *config.StoreInterval)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// 	return sdw, nil
+	// } else
+	if len(*server.Config.StoreFile) > 0 {
 		sw, err := NewSnapshotWriter(store, *server.Config.StoreFile, *config.StoreInterval)
 		if err != nil {
 			return nil, err
